b8de9f56ee4b6cb060fb39358160624d2ffef832
Martin Storme <martin.ekontakt@outlook.com>
2026-08-29 15:20:50 +0200

    Update v02_oui.py

--- v02_oui.py
+++ v02_oui.py
@@ -1,5 +1,7 @@
-# Jag gjorde först övningen i Cisco Packet Tracer och körde "show cac address-table" på enheterna som fanns där, men eftersom CPT endaast har Cisco-hårdvara i sin simulering så blev uppgiften lite enahanda i sin output.
-# För att övningen skulle bli mer varierad letade jag på nätet upp några andra leverantörer och deras tillhörande MAC-adresser (första halvan=vendor-delen) och klistrade in de leverantörer med deras tillhörande MAC/vendor-del. https://maclookup.app/
+# Jag gjorde först övningen i Cisco Packet Tracer och körde "show cac address-table" på enheterna som fanns där, 
+# men eftersom CPT endaast har Cisco-hårdvara i sin simulering så blev uppgiften lite enahanda i sin output.
+# För att övningen skulle bli mer varierad letade jag på nätet upp några andra leverantörer och deras tillhörande MAC-adresser (första halvan=vendor-delen) 
+# och klistrade in de leverantörer med deras tillhörande MAC/vendor-del. https://maclookup.app/
 
 vendors = {
     "00:11:0A": "Hewlett Packard",
